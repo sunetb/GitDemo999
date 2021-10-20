31bb7323787eac7ebad7cb773e76b44350d400f3
sune <sunetb@gmail.com>
2021-10-20 11:13:59 +0200

    Tilføjet testmetode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-    }
-    void hej () {
         System.out.println("hej");
+    }
+    void test (){
+
     }
 }
