68caea3ca6c7dabc8bd79906afbff25ddaaaeeeb
sune <sunetb@gmail.com>
2022-03-16 11:02:53 +0100

    arbejde

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,37 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hej");
-    }
-   
-    void test (){
 
+        String hemmeligtOrd = "køkkenmødding";
+        String brugteBogstaver = "";
+
+        String brugerensGæt = gæt();
+
+        boolean findes = hemmeligtOrd.contains(brugerensGæt);
+        int index = hemmeligtOrd.indexOf("k");
+        brugteBogstaver += brugerensGæt;
+        System.out.println(index);
+
+        int antal = antalForekomster(hemmeligtOrd, "k");
+        System.out.println(antal);
     }
 
-    //videe medmaster
+    private static String gæt() {
+        return "ø";
+    }
+
+    //Demo: indexOf
+    static int antalForekomster (String ord, String bogstav){
+        int ix = 0;
+        int antal = 0;
+        while ((ix = ord.indexOf(bogstav, ix)) != -1) {//Tildeling OG sammenligning på én gang!
+            antal++;
+            ix++;
+            System.out.println("hop "+ix);
+        }
+        System.out.println("Antal "+bogstav + "'er: "+antal);
+        return antal;
+    }
+
+
 }
